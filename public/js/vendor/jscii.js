4099a320b6bc920b6fabd72ab91f868c3edba49a
bgrins <briangrinstead@gmail.com>
2012-11-09 18:13:15 -0600

    express generated app, pulled in dependancies

---
+++ public/js/vendor/jscii.js
@@ -0,0 +1,142 @@
+!function() {
+
+  navigator.getMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+  var imgCanvas = document.createElement('canvas');
+  var imgCtx = imgCanvas.getContext('2d');
+  var videoCanvas = document.createElement('canvas');
+  var videoCtx = videoCanvas.getContext('2d');
+
+  var videoWidth, videoHeight, video, container, stream, videoTimer;
+
+  /**
+   * value to character mapping from dark to light
+   * add more characters and they will be accounted for automatically
+   * note: the extra &nbsp; is to account for the value range inclusive of 100%
+   */
+  var chars = ['@','#','$','=','*','!',';',':','~','-',',','.','&nbsp;', '&nbsp;'];
+  var charLen = chars.length-1;
+  function getChar(val) { return chars[parseInt(val*charLen, 10)]; }
+
+  /**
+   * log when getUserMedia or when video metadata loading fail
+   */
+  function logError(err) { if(console && console.log) console.log('Error!', err); return false; }
+
+  /**
+   * Sets the video dimension (and subsequently ASCII string dimension)
+   */
+  function setVideoDimension(width, height) {
+    videoCanvas.width = videoWidth = width;
+    videoCanvas.height = videoHeight = height;
+  }
+
+  /**
+   * given a video object and DOM element, render the ASCII string inside element
+   */
+  function renderVideo(videoEl, containerEl, cb) {
+    cb = cb || function() { };
+    if(typeof navigator.getMedia !== 'function') {
+      var msg = 'Error: browser does not support getUserMedia';
+      containerEl.innerHTML = msg;
+      return logError(msg);
+    }
+
+    video = videoEl;
+    container = containerEl;
+    navigator.getMedia({video: true, audio: true}, function(localMediaStream){
+      stream = localMediaStream;
+      var url = window.URL || window.webkitURL;
+      video.src = url.createObjectURL(localMediaStream);
+
+      startRender(15);
+      video.onloadedmetadata = logError;
+
+      cb();
+    }, logError);
+    return true;
+  }
+
+  /**
+   * gets video image data, perform ascii conversion, append string to container
+   */
+  function startRender(interval) {
+    if(typeof interval !== 'number') interval = 20;
+    videoTimer = setInterval(function(){
+      if(stream) {
+        var w = videoWidth, h = videoHeight;
+        videoCtx.drawImage(video, 0, 0, w, h);
+        var data = videoCtx.getImageData(0, 0, w, h).data;
+        container.innerHTML = getAsciiString(data, w, h);
+      }
+    }, interval);
+  }
+
+  /**
+   * Allow pause and play for ascii rendering
+   */
+  function stopRender() { if(videoTimer) clearInterval(videoTimer); }
+
+  /**
+   * given an image object and DOM element, render the ASCII string inside element
+   */
+  function renderImage(image, container) {
+
+    function drawImage(image) {
+      var ratio = image.width/image.height;
+      imgCanvas.width = w = 150;
+      imgCanvas.height = h = w/ratio;
+      imgCtx.drawImage(image, 0, 0, w, h);
+      data = imgCtx.getImageData(0, 0, w, h).data;
+      container.innerHTML = getAsciiString(data, w, h);
+    }
+
+    if (image.tagName == "CANVAS") {
+      drawImage(image);
+    }
+    else {
+      image.addEventListener('load', function(){
+        drawImage(image);
+      });
+    }
+  }
+
+  /**
+   * helper function to retrieve rgb value from pixel data
+   * (pixel data is a 1-dimensional array of rgba sequence)
+   */
+  function getRGB(d, i) { return [d[i=i*4], d[i+1], d[i+2]]; }
+
+  /**
+   * given a picture/frame's pixel data and a defined width and height
+   * return the ASCII string representing the image
+   */
+  function getAsciiString(d, width, height) {
+    var len = width*height-1, str = '';
+    for(var i=0; i<len; i++) {
+      if(i%width === 0) str += '<br>';
+      var rgb = getRGB(d, i);
+      var val = Math.max(rgb[0], rgb[1], rgb[2])/255;
+      //str += '<b style="color: rgb('+rgb.join(',')+')">'+getChar(val)+'</b>';
+      str += getChar(val);
+    }
+    return str;
+  }
+
+  /**
+   * default video dimension at 150 width and a 4:3 ratio
+   */
+  setVideoDimension(150, parseInt(150*3/4, 10));
+
+  window.Jscii = {
+    setVideoDimension: setVideoDimension,
+    renderVideo: renderVideo,
+    startRender: startRender,
+    stopRender: stopRender,
+    renderImage: renderImage
+  };
+
+}();
--- server.js
+++ server.js
@@ -1,7 +1,35 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    partials = require('express-partials'),
+    indexRoute = require('./routes/index'),
+    userRoute = require('./routes/user');
 
-console.log('Server running at http://0.0.0.0:8000/');
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.set('layout', 'views/layout.ejs');
+  app.use(partials());
+
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.cookieParser('secret?'));
+  app.use(express.session());
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', indexRoute.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
