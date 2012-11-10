fe08aeb925bb63c9b75b62081bab636e48da5b7b
bgrins <briangrinstead@gmail.com>
2012-11-09 18:40:46 -0600

    add some nasty code for webgl

--- public/js/app.js
+++ public/js/app.js
@@ -13,3 +13,50 @@ var App = new (Backbone.View.extend({
     }
 
 }))();
+
+function init(image) {
+
+    var placeholder = $("#gl-container");
+    var asciiContainer= $("#imgascii");
+
+    placeholder.offset(asciiContainer.offset());
+    var asciiWidth = asciiContainer.width();
+    $(image).width(asciiWidth);
+
+    try {
+        var canvas = fx.canvas();
+    }
+    catch (e) {
+        placeholder.html(e);
+        return;
+    }
+
+    // Create a texture from the image and draw it to the canvas
+    var texture = canvas.texture(image);
+    canvas.draw(texture).update();
+
+    placeholder.append(canvas);
+
+    var distorting = true;
+    // Draw a swirl under the mouse
+    $(placeholder).click(function(e) {
+        distorting = !distorting;
+    });
+
+    $(placeholder).mousemove(function(e) {
+        if (distorting) {
+            var offset = $(canvas).offset();
+            var x = e.pageX - offset.left;
+            var y = e.pageY - offset.top;
+            canvas.draw(texture).swirl(x, y, 200, 4).update();
+            Jscii.renderImage(canvas, asciiContainer[0]);
+        }
+    });
+
+    $(image).hide();
+}
+
+
+$("#logo").on("load", function() {
+    init(this);
+});
